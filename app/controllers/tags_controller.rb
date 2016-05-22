68c37b1672443d92a9bb0c59f03ad9f22392e32f
Martina Simicic <martina.simicic@gmail.com>
2016-05-22 12:02:47 +0200

    basic new and create for tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,4 +3,25 @@ class TagsController < ApplicationController
   def index
     @tags = Tag.all
   end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+
+    if @tag.save
+      flash[:notice] = 'Successfully saved!'
+      redirect_to tags_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:label)
+  end
 end
--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -2,9 +2,34 @@ require 'rails_helper'
 
 RSpec.describe TagsController, type: :controller do
   describe '#index' do
-    it 'render index template' do
+    it 'render :index template' do
       get :index
       expect(response).to render_template(:index)
+    end
+  end
+
+  describe '#new' do
+    it 'render :new template' do
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe '#create' do
+    context 'when tag is valid' do
+      let(:tags_params) { attributes_for :tag }
+
+      it 'creates tag record' do
+        expect { post :create, tag: tags_params }.to change{ Tag.count }.by(1)
+      end
+    end
+
+    context 'when tag is not valid' do
+      let(:tags_params) { { label: nil } }
+
+      it 'creates tag record' do
+        expect { post :create, tag: tags_params }.to change{ Tag.count }.by(0)
+      end
     end
   end
 end
