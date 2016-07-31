c874e75bdcb2e3d1ef6fd536b09d52d8c82f024b
Martina Simicic <martina.simicic@gmail.com>
2016-07-31 16:45:36 +0200

    Correct the name of migration images instead of photos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160522103332) do
+ActiveRecord::Schema.define(version: 20160731143355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20160522103332) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.integer  "event_id"
+    t.integer  "position"
   end
 
   add_index "images", ["tag_id"], name: "index_images_on_tag_id", using: :btree
