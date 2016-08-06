f883edcc85b3f960fb2f2f68cea8b7df525ad6dd
Martina Simicic <martina.simicic@gmail.com>
2016-08-06 10:43:22 +0200

    Add selection buttons for the images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,29 @@
 class ImagesController < ApplicationController
 
   def show
-    @image = Image.find(params[:id])
+    @image = load_image
+    @event = load_event
+    @tags = Tag.all
+  end
+
+  def update
+    @image = load_image
+    @image.update_attributes(images_params)
+    @tags = Tag.all
+    @event = load_event
+  end
+
+  private
+
+  def load_event
+    @event = Event.find(params[:event_id])
+  end
+
+  def load_image
+    Image.find(params[:id])
+  end
+
+  def images_params
+    params.require(:image).permit(:tag_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :tags
   resources :events do
-    resources :images, only: [:show]
+    resources :images, only: [:show, :update]
   end
 end
